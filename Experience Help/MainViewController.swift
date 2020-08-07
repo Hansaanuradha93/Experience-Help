4ae1dc3a830aa46009358ad8a104cde896128f65
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-07 21:10:41 +0530

    alert added

--- Experience Help/MainViewController.swift
+++ Experience Help/MainViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import MessageUI
+
 
 class MainViewController: UIViewController {
     
@@ -11,14 +13,34 @@ class MainViewController: UIViewController {
     
 
     @IBAction func NikeButtonPressed(_ sender: Any) {
-        guard let mobileNumber = mobileNumberTextField.text, !mobileNumber.isEmpty else { return }
-        facetime(phoneNumber: mobileNumber)
+//        guard let mobileNumber = mobileNumberTextField.text, !mobileNumber.isEmpty else { return }
+//        facetime(phoneNumber: mobileNumber)
+        presentAlert(sender)
     }
     
     
     fileprivate func facetime(phoneNumber:String) {
         guard let faceTimeUrl = URL(string: "facetime-audio://\(phoneNumber)") else { return }
         UIApplication.shared.open(faceTimeUrl)
+    }
+    
+    fileprivate func presentAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Get Help", message: "Please call or send a text message to our receptionist", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Call", style: .default, handler: { (action) in
+            print("Call")
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Send message", style: .default, handler: { (action) in
+            print("Send Message")
+        }))
+
+        if let popoverController = alert.popoverPresentationController {
+            popoverController.sourceView = self.view
+            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
+        self.present(alert, animated: true, completion: nil)
+        
     }
 }
 
