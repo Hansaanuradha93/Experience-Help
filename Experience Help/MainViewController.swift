d2ea599951725e5afe09a101bd8011f0e0636967
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-07 20:28:54 +0530

    facetime audio call function added

--- Experience Help/MainViewController.swift
+++ Experience Help/MainViewController.swift
@@ -1,13 +1,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var mobileNumberTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 
     @IBAction func NikeButtonPressed(_ sender: Any) {
-        UIApplication.shared.open(URL(string:"facetime://Experience")! as URL, options: [:], completionHandler: nil)
+        guard let mobileNumber = mobileNumberTextField.text, !mobileNumber.isEmpty else { return }
+        facetime(phoneNumber: mobileNumber)
+    }
+    
+    
+    fileprivate func facetime(phoneNumber:String) {
+        guard let faceTimeUrl = URL(string: "facetime-audio://\(phoneNumber)") else { return }
+        UIApplication.shared.open(faceTimeUrl)
     }
 }
 
