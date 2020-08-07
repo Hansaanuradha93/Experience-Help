a641cc2f403636d79193ed2906ffb5295d76e4a4
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-08-07 21:27:54 +0530

    send text messaging added

--- Experience Help/MainViewController.swift
+++ Experience Help/MainViewController.swift
@@ -1,7 +1,6 @@
 import UIKit
 import MessageUI
 
-
 class MainViewController: UIViewController {
     
     @IBOutlet weak var mobileNumberTextField: UITextField!
@@ -23,15 +22,15 @@ class MainViewController: UIViewController {
     }
     
     fileprivate func presentAlert(_ sender: Any) {
+        guard let mobileNumber = self.mobileNumberTextField.text, !mobileNumber.isEmpty else { return }
         let alert = UIAlertController(title: "Get Help", message: "Please call or send a text message to our receptionist", preferredStyle: .actionSheet)
         
         alert.addAction(UIAlertAction(title: "Call", style: .default, handler: { (action) in
-            guard let mobileNumber = self.mobileNumberTextField.text, !mobileNumber.isEmpty else { return }
             self.facetime(phoneNumber: mobileNumber)
         }))
         
         alert.addAction(UIAlertAction(title: "Send message", style: .default, handler: { (action) in
-            print("Send Message")
+            self.send(mobileNumber: mobileNumber, message: "Predefined Message")
         }))
 
         if let popoverController = alert.popoverPresentationController {
@@ -41,7 +40,35 @@ class MainViewController: UIViewController {
         }
         self.present(alert, animated: true, completion: nil)
     }
+    
+    
+    fileprivate func send(mobileNumber: String, message: String) {
+        let messageVC = MFMessageComposeViewController()
+        messageVC.body = message;
+        messageVC.recipients = [mobileNumber]
+        messageVC.messageComposeDelegate = self
+        if MFMessageComposeViewController.canSendText() {
+            self.present(messageVC, animated: true, completion: nil)
+        }
+    }
 }
 
+
+extension MainViewController: MFMessageComposeViewControllerDelegate {
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch (result) {
+        case .cancelled:
+            print("Message was cancelled")
+        case .failed:
+            print("Message failed")
+        case .sent:
+            print("Message was sent")
+        default:
+            return
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
 
 
